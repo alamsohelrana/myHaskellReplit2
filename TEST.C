eccad731f9a30679edc447128b94a6068bdf4fe5
alamsohelrana <alam.sohel1990@gmail.com>
2022-07-11 19:04:22 +0530

    Testing

---
+++ TEST.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*This is a comment*/
+
+int main(){
+
+    printf("Hello World1 \n");
+    printf("Hello World2");
+    return 0;
+    printf("Hello World3");
+
+}
+
+int adding(int x , int y){
+
+    return (x+y);
+}
